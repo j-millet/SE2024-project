4714f269bf9de76b39146759c2e7a81569772c1b
KubaCzech <150391696+KubaCzech@users.noreply.github.com>
2024-05-19 23:06:23 +0200

    Add files via upload

--- src/main/java/pl/put/poznan/jsontools/logic/IncludeKeysJsonTool.java
+++ src/main/java/pl/put/poznan/jsontools/logic/IncludeKeysJsonTool.java
@@ -8,16 +8,33 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * This is the class that returns json string only with keys provided by the user (filtering)
+ * @author Kuba Czech
+ * @author Jakub Jagla
+ * @author Lukasz Borak
+ */
 public class IncludeKeysJsonTool extends JsonTool {
     private String[] keysToInclude;
     private ObjectMapper objectMapper;
 
+    /**
+     * Constructor for class IncludeKeysJsonTool
+     * @param wrappee json format (input) string
+     * @param keysToInclude array of strings that contains keys to be included
+     */
     public IncludeKeysJsonTool(IJsonTool wrappee, String[] keysToInclude) {
         super(wrappee, "include");
         this.keysToInclude = keysToInclude;
         this.objectMapper = new ObjectMapper();
     }
 
+    /**
+     *
+     * @param jsonString string to be filtered
+     * @return json string with only certain keys included
+     * @throws Exception
+     */
     private String includeKeys(String jsonString) throws Exception {
         Map<String, Object> map = objectMapper.readValue(jsonString, new TypeReference<Map<String, Object>>() {
         });
@@ -28,6 +45,10 @@ public class IncludeKeysJsonTool extends JsonTool {
         return objectMapper.writeValueAsString(filteredMap);
     }
 
+    /**
+     * Inherited method (from Json Tool Class) that returns string after transformation
+     * @return string in json format that is result of the filtering
+     */
     @Override
     public String getJsonString() {
         try {
