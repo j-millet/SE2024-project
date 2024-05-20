110b35ce63b478457d2661d69fafba78fc870ff7
B0cz3k <lukasz.borak03@gmail.com>
2024-05-20 13:59:15 +0200

    added jsonCompare functionality, cosmetic changes

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,29 +3,38 @@ document.getElementById('submit-button').addEventListener('click', function() {
     const operation = document.getElementById('operation-select').value;
     const paramsInput = document.getElementById('params-input').value;
 
-    const params = paramsInput.split(',')
-        .map(param => param.trim())
-        .filter(param => param.length > 0);
+    function processJsonInput(json) {
+        let jsonOutput;
+        try {
+            let input = String.raw`${json.replace(/"/g, "\\\"").replace(/'/g, String.raw`<sq>`).replace(/(\r\n|\n|\r)/gm, "")}`;
+            console.log("Processed input:", JSON.stringify(input));
 
-    let json;
-    try {
-        var input = String.raw`${jsonInput.replace(/"/g,"\\\"").replace(/'/g,String.raw`<sq>`).replace(/(\r\n|\n|\r)/gm, "")}`;
-        console.log(JSON.stringify(input));
-        json = JSON.parse('"' + input + '"');
-        console.log(json);
-    } catch (e) {
-        alert("Invalid JSON");
-        console.error("Invalid JSON");
-        console.error(e);
-        return;
+            jsonOutput = JSON.parse('"' + input + '"');
+            console.log("Parsed JSON:", jsonOutput);
+        } catch (e) {
+            alert("Invalid JSON");
+            console.error("Invalid JSON");
+            console.error(e);
+            return;
+        }
+        return jsonOutput;
     }
-    console.log(json);
+
+    let params;
+    if (paramsInput.charAt(0) === '{') {
+        params = [processJsonInput(paramsInput)];
+    } else {
+        params = paramsInput.split(',')
+            .map(param => param.trim())
+            .filter(param => param.length > 0);
+    }
+    console.log(params);
 
     const requestBody = {
-        "json-string": json,
+        "json-string": processJsonInput(jsonInput),
         "params": params
     };
-
+    console.log(requestBody);
     fetch(`http://localhost:8080/api/${operation}`, {
         method: 'POST',
         headers: {
@@ -35,7 +44,7 @@ document.getElementById('submit-button').addEventListener('click', function() {
     })
         .then(response => response.text())
         .then(text => {
-            var outerJSON = JSON.parse(text);
+            let outerJSON = JSON.parse(text);
             if ("error" in outerJSON) {
                 alert(outerJSON["error"]);
                 return;
@@ -45,4 +54,31 @@ document.getElementById('submit-button').addEventListener('click', function() {
         .catch(error => {
             console.error('Fetch error:', error);
         });
+});
+
+document.getElementById('operation-select').addEventListener('change', function () {
+    const operation = document.getElementById('operation-select');
+    const paramsInput = document.getElementById('params-input');
+    const operationsWithoutParams = ['identity', 'minify', 'full'];
+
+    paramsInput.value = '';
+
+    if (operationsWithoutParams.includes(operation.value)) {
+        paramsInput.placeholder = '';
+        paramsInput.disabled = true;
+        paramsInput.classList.add('disabled');
+    } else {
+        paramsInput.disabled = false;
+        paramsInput.classList.remove('disabled');
+        if (operation.value === 'compare') {
+            paramsInput.placeholder = 'Enter valid JSON to compare';
+        } else {
+            paramsInput.placeholder = 'Enter parameters (comma separated)';
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const event = new Event('change');
+    document.getElementById('operation-select').dispatchEvent(event);
 });
